9629fbc93713f4a76820ac8fb631015ba3db7220
Guilherme Henrique Fortes Trindade Filho <78531449+GuilleOne@users.noreply.github.com>
2021-05-17 22:58:24 -0300

    Fundo sem foco

--- src/jogo/Menu.java
+++ src/jogo/Menu.java
@@ -1,20 +1,29 @@
 package jogo;
 
 
-
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Menu  extends JPanel{
-	
 
-	public Menu() {
+
+	public Menu() throws IOException {
+
+		BufferedImage imagem = ImageIO.read(new File("img\\menu fundo.png"));
+		JLabel fundo = new JLabel(new ImageIcon(imagem));
+		//Tamanho fundo
+		fundo.setBounds(0, 0, 1280, 720);
 		
 		
 		// Configs botões
@@ -27,11 +36,13 @@ public class Menu  extends JPanel{
 		bttini.setBounds(450, 150, 310, 74);
 		bttins.setBounds(450, 300, 310, 74);
 		bttsair.setBounds(450, 450, 310, 74);
-
+		
 		bttsair.setContentAreaFilled(false);
 		bttsair.setBorderPainted(true);
 		bttsair.setBorder(null);
-
+		
+		
+		
 		bttins.setContentAreaFilled(false);
 		bttins.setBorderPainted(true);
 		bttins.setBorder(null);
@@ -40,45 +51,49 @@ public class Menu  extends JPanel{
 		bttini.setBorderPainted(true);
 		bttini.setBorder(null);
 
-		
 
+		
+		
+		
+		this.add(fundo);
 		this.add(bttini);
 		this.add(bttins);
 		this.add(bttsair);
 		this.setLayout(null);
-
+		this.setBackground(Color.gray);
+		
 		bttini.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				
+
+
 				try {
 					Jogo jogo = new Jogo();
 					Janela.telas.add(jogo, "Tela Jogo");
 					Janela.controleTela.show(Janela.telas, "Tela Jogo");
-					
-					
-					
-					
+
+
+
+
 				} catch (UnsupportedAudioFileException | IOException | LineUnavailableException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-	
-			
+
+
 			}
 		});
 		bttins.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				Instrucao instrucao = new Instrucao ();
-	            Janela.telas.add(instrucao, "Instrução");
-	            Janela.controleTela.show(Janela.telas, "Instrução");
 
-				
+				Instrucao instrucao = new Instrucao ();
+				Janela.telas.add(instrucao, "Instrução");
+				Janela.controleTela.show(Janela.telas, "Instrução");
+
+
 			}
 
 		});
@@ -93,8 +108,15 @@ public class Menu  extends JPanel{
 
 		});
 
-
 	}
+
 	
 
-}
+
+	}
+
+
+
+
+
+
